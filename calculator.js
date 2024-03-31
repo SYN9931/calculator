281561745e4b152df5f6bd72d83beee5c9acd1b0
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2024-04-01 05:54:16 +0800

    added basic calculator functions
    added sum, subtract, multiply, and divide functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+//sum
+const add = function (a,b){
+    return a+b;
+}
+
+//subtract
+const subtract = function (a,b){
+    return a-b;
+}
+
+//multiply
+const multiply = function (a,b){
+    return a*b;
+}
+
+//divide 
+const divide = function (a,b){
+    return a/b;
+}
+
+
