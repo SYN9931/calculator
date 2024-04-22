e90f46b081be7efa7025d3c4c2f2dc2ddc235cdb
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2024-04-23 05:47:39 +0800

    added data--action attribute to html describes what each button does

--- calculator.js
+++ calculator.js
@@ -19,14 +19,3 @@ const divide = function (a,b){
     return a/b;
 }
 
-//display on screen button click onto html screen
-const screen = document.getElementsById("calculator_display");
-const allButtons = document.getElementsByClassName('numpad');
-
-Array.prototype.forEach.call(allButtons, (e) => e.addEventListener('click', () => calculator_display.innerHTML += e.innerHTML));
-
-//clear everything on calculator_display
-
-function clearInput(){
-    document.getElementById("calculator_display").value="";
-};
