af383c6bcae9a244d35e39501d00c60b1b704ab2
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2024-04-24 05:51:27 +0800

    access data-action attribute
    to determine type of key being pressed

--- calculator.js
+++ calculator.js
@@ -20,10 +20,23 @@ const divide = function (a,b){
 }
 
 const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('calculator_keys')
+const keys = calculator.querySelector('.calculator_keys')
 
 keys.addEventListener('click', e => {
     if(e.target.matches('button')) {
 //action
+
+        const key = e.target
+        const action = key.dataset.action
+
+        if (!action){
+            console.log('number key!')
+        }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {console.log('operator key!')}
     }
 })
