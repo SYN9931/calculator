778473e1eb125182489d336536c210a3236bd349
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2024-04-24 05:25:22 +0800

    add event listener to listen for keypresses

--- calculator.js
+++ calculator.js
@@ -19,3 +19,11 @@ const divide = function (a,b){
     return a/b;
 }
 
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('calculator_keys')
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+//action
+    }
+})
