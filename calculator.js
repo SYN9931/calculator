66b6de829293a9cad0c3ea677b2c22e7ac7a6e56
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2024-04-25 05:55:51 +0800

    display number keys when clicked

--- calculator.js
+++ calculator.js
@@ -21,6 +21,7 @@ const divide = function (a,b){
 
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator_keys')
+const display = document.querySelector('.calculator_display')
 
 keys.addEventListener('click', e => {
     if(e.target.matches('button')) {
@@ -28,9 +29,16 @@ keys.addEventListener('click', e => {
 
         const key = e.target
         const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
 
         if (!action){
             console.log('number key!')
+            if (displayedNum === '0'){
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
         }
         if (
             action === 'add' ||
@@ -38,5 +46,17 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
         ) {console.log('operator key!')}
+        if (
+            action === 'decimal') {
+            console.log('decimal key!')
+        }
+        if (
+            action === 'clear') {
+            console.log ('clear key!')
+        }
+        if (
+            action === 'calculate') {
+            console.log('equal key!')
+        }
     }
 })
